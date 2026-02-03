73e8be6dbc6a9545c4444b294b41bea907ba70e7
JuJust123 <justin.esquer@mmibordeaux.com>
2026-02-03 16:16:36 +0100

    All comments

--- api/comments/delete.php
+++ api/comments/delete.php
@@ -3,6 +3,17 @@ session_start();
 require_once $_SERVER['DOCUMENT_ROOT'] . '/config.php';
 require_once $_SERVER['DOCUMENT_ROOT'] . '/functions/ctrlSaisies.php';
 
+// Suppression tous les commentaires d'un membre
+if ($_SERVER['REQUEST_METHOD'] === 'GET' && isset($_GET['numMemb']) && !isset($_GET['numCom'])) {
+    $numMemb = (int) ($_GET['numMemb'] ?? 0);
+
+    if ($numMemb > 0) {
+        sql_delete('COMMENT', 'numMemb = ' . $numMemb);
+        header('Location: /views/backend/members/delete.php?numMemb=' . $numMemb . '&success=' . urlencode('Tous les commentaires du membre ont été supprimés'));
+        exit;
+    }
+}
+
 if (
     empty($_POST['numCom']) ||
     empty($_POST['redirect'])
